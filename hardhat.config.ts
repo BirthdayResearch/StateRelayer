c0a0a43db323417b27dc1700ce50f88101831a87
Abhishek <abhishekkochar2@gmail.com>
2023-05-31 12:17:01 +0700

    Configure solhint, eslint and prettier

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,9 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox';
+
+import { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
   typechain: {
     outDir: './generated',
     target: 'ethers-v5',
--- tests/StateRelayer.deployment.spec.ts
+++ tests/StateRelayer.deployment.spec.ts
@@ -1,24 +1,24 @@
-
-import {expect} from "chai"
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
-import {deployContract} from './utils/deployment'
-import { StateRelayer } from "../generated";
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { expect } from 'chai';
 
-describe("State relayer deployment test", () => {
-    let stateRelayerProxy:StateRelayer;
-    let admin: SignerWithAddress;
-    let bot: SignerWithAddress;
+import { StateRelayer } from '../generated';
+import { deployContract } from './utils/deployment';
 
-    it("Admin address should be admin address signer", async() => {
-        ({stateRelayerProxy, admin} = await loadFixture(deployContract))
-        const adminRole = await stateRelayerProxy.DEFAULT_ADMIN_ROLE()
-        expect(await stateRelayerProxy.hasRole(adminRole, admin.address)).to.be.equal(true)
-    })
-    
-    it("Bot address should be bot address signer", async() => {
-        ({stateRelayerProxy, bot} = await loadFixture(deployContract))
-        const botRole = await stateRelayerProxy.BOT_ROLE()
-        expect(await stateRelayerProxy.hasRole(botRole, bot.address)).to.be.equal(true)
-    })
-})
+describe('State relayer deployment test', () => {
+  let stateRelayerProxy: StateRelayer;
+  let admin: SignerWithAddress;
+  let bot: SignerWithAddress;
+
+  it('Admin address should be admin address signer', async () => {
+    ({ stateRelayerProxy, admin } = await loadFixture(deployContract));
+    const adminRole = await stateRelayerProxy.DEFAULT_ADMIN_ROLE();
+    expect(await stateRelayerProxy.hasRole(adminRole, admin.address)).to.be.equal(true);
+  });
+
+  it('Bot address should be bot address signer', async () => {
+    ({ stateRelayerProxy, bot } = await loadFixture(deployContract));
+    const botRole = await stateRelayerProxy.BOT_ROLE();
+    expect(await stateRelayerProxy.hasRole(botRole, bot.address)).to.be.equal(true);
+  });
+});
