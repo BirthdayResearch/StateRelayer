a28c486067cd239b67000f0afa847790927d8e62
Abhishek <abhishekkochar2@gmail.com>
2023-06-14 17:07:15 +0800

    Script for deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,19 @@ import '@nomicfoundation/hardhat-toolbox';
 
 import { HardhatUserConfig } from 'hardhat/config';
 
+require('dotenv').config({
+  path: '.env',
+});
+
 const config: HardhatUserConfig = {
   solidity: '0.8.18',
+  networks: {
+    floopyTestnet: {
+      url: 'http://35.187.53.161:20551',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 1133,
+    },
+  },
   typechain: {
     outDir: './generated',
     target: 'ethers-v5',
