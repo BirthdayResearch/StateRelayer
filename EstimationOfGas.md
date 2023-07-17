662812eede631b89771714131a5639b00387d699
cuongquangnam <cuong@cakedefi.com>
2023-07-17 16:24:45 +0800

    estimation

---
+++ EstimationOfGas.md
@@ -0,0 +1,53 @@
+Basically, the cost of our transaction = effective gas price \* gasUsed
+
+## Gas used during a successful transaction:
+
+1. These intrinsic gas are consumed first:
+
+![Intrinsic Gas](./img/Pasted%20Graphic.png)
+
+Some note:
+
+- For our transactions to update via the three update functions, T_A is an empty set
+
+- There is no G_txcreate in our updating transactions
+
+- Therefore, we only care about G_txdatazero and G_txdatanonzero.
+
+2. Then the gas cost is spent on executing every opcode after this (except for the case of creation transaction, the gas cost will be plus the code_deposit because of the creation of the contract)
+
+3. Along the way, there is going to be a refund for any empty of storage or self-destruct of an account. This refund is reimbursed at the end of the execution and capped at half of the gas spent for the whole transaction.
+
+According to our implementation up till now, there seems to be only 2 concerns:
+
+1. The non-zero-ness or not of the bytes in our call data
+
+   Let’s assume all bytes are non-zero during our gas estimation for the worst case
+
+2. The SSTORE dynamic charging.
+
+   I think we can assume for most of the cases, the gas cost is going to be switching a non-zero slot to a non-zero slot.
+   Therefore, during our estimation, let's take the transition from non-zero to non-zero of the values.
+
+In conclusion, during our estimation, let's consider the case for switching from non-zero data to non-zero data, and then pushing the value to where all calldata bytes of the transaction are non-zero.
+
+Reference:
+
+https://ethereum.github.io/yellowpaper/paper.pdf
+
+https://github.com/wolflo/evm-opcodes/tree/main
+
+## Regarding effective gas price
+
+The type of transaction we are sending is of EIP-1559, hence
+
+Effective gas price = min (max_fee_per_gas, block_base_fee + max_priority_fee_per_gas)(
+[reference](https://github.com/ethereum/EIPs/blob/2e05704d86356ff23eb5eb7136e5d9fc7eac0545/EIPS/eip-1559.md?plain=1#L224)
+)
+
+max_fee_per_gas, block_base_fee and max_priority_fee_per_gas seems all depend on the market condition. One reference for such market that we can take is Ethereum mainnet.
+
+Link: https://etherscan.io/chart/gasprice
+
+A calculation of the average gas price for the last 6 months indicates a value of 40 gWei.
+Therefore, let's put the gas price for our one tx = 125 % \* 40 = 50
---
+++ scripts/gasEstimation2.ts
@@ -0,0 +1,187 @@
+import { BigNumber as BigFloatingNumber } from 'bignumber.js';
+import { BigNumber } from 'ethers';
+
+import { deployContract } from '../tests/utils/deployment';
+
+// dex names crawled from the mainnet defichain
+const dexesNames = [
+  'dETH-DFI',
+  'dBTC-DFI',
+  'dUSDT-DFI',
+  'dDOGE-DFI',
+  'dLTC-DFI',
+  'dBCH-DFI',
+  'dUSDC-DFI',
+  'DUSD-DFI',
+  'dBABA-DUSD',
+  'dPLTR-DUSD',
+  'dAAPL-DUSD',
+  'dSPY-DUSD',
+  'dQQQ-DUSD',
+  'dPDBC-DUSD',
+  'dVNQ-DUSD',
+  'dARKK-DUSD',
+  'dGLD-DUSD',
+  'dURTH-DUSD',
+  'dTLT-DUSD',
+  'dSLV-DUSD',
+  'dEEM-DUSD',
+  'dNVDA-DUSD',
+  'dCOIN-DUSD',
+  'dMSFT-DUSD',
+  'dNFLX-DUSD',
+  'dVOO-DUSD',
+  'dFB-DUSD',
+  'dDIS-DUSD',
+  'dMCHI-DUSD',
+  'dMSTR-DUSD',
+  'dINTC-DUSD',
+  'dPYPL-DUSD',
+  'dBRK.B-DUSD',
+  'dPG-DUSD',
+  'dKO-DUSD',
+  'dSAP-DUSD',
+  'dGSG-DUSD',
+  'dURA-DUSD',
+  'dCS-DUSD',
+  'dAMZN-DUSD',
+  'dPPLT-DUSD',
+  'dTAN-DUSD',
+  'dXOM-DUSD',
+  'dGOVT-DUSD',
+  'dGOOGL-DUSD',
+  'dUSDT-DUSD',
+  'dUSDC-DUSD',
+  'dGME-DUSD',
+  'dDAX-DUSD',
+  'dJNJ-DUSD',
+  'dADDYY-DUSD',
+  'dGS-DUSD',
+  'dTSLA-DUSD',
+  'dWMT-DUSD',
+  'dUL-DUSD',
+  'dUNG-DUSD',
+  'dUSO-DUSD',
+  'csETH-dETH',
+  'dARKX-DUSD',
+  'dXLRE-DUSD',
+  'dVBK-DUSD',
+  'dXLE-DUSD',
+  'dNSRGY-DUSD',
+  'dSHEL-DUSD',
+  'dSH-DUSD',
+  'dBITI-DUSD',
+  'dEUROC-DFI',
+  'dEUROC-DUSD',
+  'dBURN2-DUSD',
+];
+
+// to run this file, run npx hardhat clean && npm i && npx hardhat run scripts/gasEstimation2.ts
+async function estimateGasCost() {
+  const dexesData: BigNumber[] = [];
+  const masterData: BigNumber[] = [];
+  const vaultData: BigNumber[] = [];
+  const burnData: BigNumber[] = [];
+  let maxDexesCallDataCost = BigNumber.from(0);
+  let maxMasterCallDataCost = BigNumber.from(0);
+  let maxVaultCallDataCost = BigNumber.from(0);
+  let maxBurnCallDataCost = BigNumber.from(0);
+  const { bot, stateRelayerProxy } = await deployContract();
+  for (let i = 1; i < 5; i += 1) {
+    const dexValues: any[] = [];
+    for (let j = 0; j < 69; j += 1) {
+      dexValues.push({
+        primaryTokenPrice: i,
+        volume24H: i,
+        totalLiquidity: i,
+        APR: i,
+        firstTokenBalance: i,
+        secondTokenBalance: i,
+        rewards: i,
+        commissions: i,
+        decimals: i,
+      });
+    }
+    const dexUpdate = await stateRelayerProxy.connect(bot).updateDEXInfo(dexesNames, dexValues, 1, 1);
+    const dexUpdateReceipt = await dexUpdate.wait();
+
+    const masterDataUpdate = await stateRelayerProxy.connect(bot).updateMasterNodeInformation({
+      totalValueLockedInMasterNodes: i,
+      zeroYearLocked: i,
+      fiveYearLocked: i,
+      tenYearLocked: i,
+      decimals: i,
+    });
+    const masterDataTxReceipt = await masterDataUpdate.wait();
+
+    const vaultDataUpdate = await stateRelayerProxy.connect(bot).updateBurnInfo({
+      fee: i,
+      auction: i,
+      payback: i,
+      emission: i,
+      total: i,
+      decimals: i,
+    });
+    const vaultTxReceipt = await vaultDataUpdate.wait();
+
+    const burnDataUpdate = await stateRelayerProxy.connect(bot).updateVaultGeneralInformation({
+      noOfVaults: i,
+      totalLoanValue: i,
+      totalCollateralValue: i,
+      totalCollateralizationRatio: i,
+      activeAuctions: i,
+      decimals: i,
+    });
+    const burnTxReceipt = await burnDataUpdate.wait();
+
+    dexesData.push(dexUpdateReceipt.gasUsed);
+    masterData.push(masterDataTxReceipt.gasUsed);
+    vaultData.push(vaultTxReceipt.gasUsed);
+    burnData.push(burnTxReceipt.gasUsed);
+    console.log('Successfully update ', i);
+    if (i === 1) {
+      maxDexesCallDataCost = BigNumber.from(((dexUpdate.data.length - 2) / 2) * 16);
+      maxMasterCallDataCost = BigNumber.from(((masterDataUpdate.data.length - 2) / 2) * 16);
+      maxVaultCallDataCost = BigNumber.from(((vaultDataUpdate.data.length - 2) / 2) * 16);
+      maxBurnCallDataCost = BigNumber.from(((burnDataUpdate.data.length - 2) / 2) * 16);
+    }
+  }
+
+  // as predicted, change from zero to non-zero incurs more cost compared to changing from non-zero to non-zero
+  // besides, because the number 2, 3, 4 are represented as
+  // 0x0000000000000000000000000000000000000000000000000000000000000002
+  // 0x0000000000000000000000000000000000000000000000000000000000000003
+  // 0x0000000000000000000000000000000000000000000000000000000000000004
+  // their representation in input data comprise the same number of non-zero / zero bytes
+  // --> updating cost between when i = 2, i = 3 and i = 4 are the same
+  console.log('Update dex data costs in gas units');
+  console.log(dexesData);
+  console.log('Update Master data costs in gas units');
+  console.log(masterData);
+  console.log('Update Vault data costs in gas units');
+  console.log(vaultData);
+  console.log('Update burn data costs in gas units');
+  console.log(burnData);
+
+  console.log(
+    'Estimated cost in DFI ',
+    // take the second element of the array
+    new BigFloatingNumber(
+      dexesData[1]
+        .add(maxDexesCallDataCost)
+        .add(masterData[1])
+        .add(maxMasterCallDataCost)
+        .add(vaultData[1])
+        .add(maxVaultCallDataCost)
+        .add(burnData[1])
+        .add(maxBurnCallDataCost)
+        .mul('50000000000') // multiply the gas units with the estimated effectiveGasPrice -- 50 gWei
+        .toString(),
+    )
+      .div(new BigFloatingNumber(10).pow(18)) // decimals of native DFI on DMC = 18
+      .multipliedBy(30 * 24) // multiply by 30 days per month, 24 hours per day.
+      .toString(),
+  );
+}
+
+estimateGasCost();
