81035f9be874f24a34bda4b2d8da324595d6ae3a
Cuong  Truong <cuong@cakedefi.com>
2023-09-11 14:57:10 +0800

    change gasEstimation3

--- scripts/gasEstimation3BatchCallByBot.ts
+++ scripts/gasEstimation3BatchCallByBot.ts
@@ -1,6 +1,5 @@
 import { BigNumber as BigFloatingNumber } from 'bignumber.js';
 
-import { mockedBurnData } from '../bot/test-i9n/mockData/oceanMockedData';
 import { StateRelayer__factory } from '../generated';
 import { deployContract } from '../tests/utils/deployment';
 
@@ -77,12 +76,6 @@ const dexesNames = [
   'dBURN2-DUSD',
 ];
 
-const tokensSymbol = mockedBurnData.tokens.map((str) => str.split('@')[1]);
-const paybackburntokensSymbol = mockedBurnData.paybackburntokens.map((str) => str.split('@')[1]);
-const dexfeetokensSymbol = mockedBurnData.paybackburntokens.map((str) => str.split('@')[1]);
-const dfipaybacktokensSymbol = mockedBurnData.dexfeetokens.map((str) => str.split('@')[1]);
-const dfip2023Symbol = mockedBurnData.dfip2203.map((str) => str.split('@')[1]);
-
 // to run this file, run npx hardhat clean && npm i && npx hardhat run scripts/gasEstimation3BatchCallByBot.ts
 async function estimateGasCost() {
   const { bot, stateRelayerProxy } = await deployContract();
@@ -116,27 +109,7 @@ async function estimateGasCost() {
       },
     ]);
 
-    const vaultDataUpdateCallData = stateRelayerInterface.encodeFunctionData('updateBurnInfo', [
-      {
-        addr: '8defichainBurnAddressXXXXXXXdRQkSm',
-        amount: i,
-        tokens: tokensSymbol.map((symbol) => ({ amount: i, token: symbol })),
-        feeburn: i,
-        emissionburn: i,
-        auctionburn: i,
-        paybackburn: i,
-        paybackburntokens: paybackburntokensSymbol.map((symbol) => ({ amount: i, token: symbol })),
-        dexfeetokens: dexfeetokensSymbol.map((symbol) => ({ amount: i, token: symbol })),
-        dfipaybackfee: i,
-        dfipaybacktokens: dfipaybacktokensSymbol.map((symbol) => ({ amount: i, token: symbol })),
-        paybackfees: [],
-        paybacktokens: [],
-        dfip2203: dfip2023Symbol.map((symbol) => ({ amount: i, token: symbol })),
-        dfip2206f: [],
-      },
-    ]);
-
-    const burnUpdateCallData = stateRelayerInterface.encodeFunctionData('updateVaultGeneralInformation', [
+    const vaultDataUpdateCallData = stateRelayerInterface.encodeFunctionData('updateVaultGeneralInformation', [
       {
         noOfVaultsNoDecimals: i,
         totalLoanValue: i,
@@ -148,7 +121,7 @@ async function estimateGasCost() {
 
     const batchCallTx = await stateRelayerProxy
       .connect(bot)
-      .batchCallByBot([dexUpdateCallData, masterDataUpdateCallData, vaultDataUpdateCallData, burnUpdateCallData]);
+      .batchCallByBot([dexUpdateCallData, masterDataUpdateCallData, vaultDataUpdateCallData]);
     const batchCallTxReceipt = await batchCallTx.wait();
 
     console.log('Successfully update ', i);
@@ -160,7 +133,7 @@ async function estimateGasCost() {
     batchCallStats.push(batchCallTxReceipt!.gasUsed);
   }
 
-  // average estimation (171 DFI)
+  // average estimation (152 DFI)
   console.log(
     'Average estimated cost in DFI ',
     // take the second element of the array
@@ -173,7 +146,7 @@ async function estimateGasCost() {
       .toString(),
   );
 
-  // worst-case estimation (568 DFI)
+  // worst-case estimation (501 DFI)
   console.log(
     'Maximum cost in DFI',
     new BigFloatingNumber(
