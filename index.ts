7a3e8443eaf6fa7dfff42a4ffa80450089cf6d16
Cuong  Truong <cuong@cakedefi.com>
2023-09-12 22:43:59 +0800

    make the repo lambda compatible

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-export { handler } from './bot/StateRelayerBot';
+import { handler } from './bot/StateRelayerBot';
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import { EnvironmentNetwork } from '@waveshq/walletkit-core';
+import {ethers} from 'ethers';
+
+exports.handler = async function (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+    const ethereumProvider = new ethers.JsonRpcProvider(process.env.ETHEREUM_PROVIDER_URL || '')
+    
+    await handler({
+        testGasCost: false, 
+        urlNetwork: process.env.DEFICHAIN_MAINNET_URL || '',
+        envNetwork: EnvironmentNetwork.MainNet,
+        contractAddress: process.env.CONTRACT_ADDRESS || '',
+        signer: new ethers.Wallet(process.env.PRIVATE_KEY || '', ethereumProvider )
+    })
+} 
